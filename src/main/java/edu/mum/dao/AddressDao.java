fd263d7e4042766f41df674e48c91e507dd95360
Administrator <Administrator@WIN7-1706051100>
2017-07-18 09:19:30 +0800

    Removed old packages

--- src/main/java/edu/mum/dao/AddressDao.java
+++
@@ -1,7 +0,0 @@
-package edu.mum.dao;
-
-import edu.mum.domain.Address2;
-
-public interface AddressDao extends GenericDao<Address2> {
-      
-}
--- src/main/java/edu/mum/domain/PaymentType.java
+++
@@ -1,5 +0,0 @@
-package edu.mum.domain;
-
-public enum PaymentType {
-	CREDITCARD, DEBITCARD, MASTERCARD,CASH
-}
