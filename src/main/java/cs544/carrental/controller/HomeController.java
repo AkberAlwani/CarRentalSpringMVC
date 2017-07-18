843b5cc01675af6e572b26573108fc04d3a4eb16
Administrator <Administrator@WIN7-1706051100>
2017-07-18 11:55:08 +0800

    Fixed the mapping issue

--- src/main/java/cs544/carrental/controller/HomeController.java
+++ src/main/java/cs544/carrental/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
  
-	@RequestMapping("/")
+	@RequestMapping({"/", "/welcome"})
 	public String welcome(Model model) {
 		
 		model.addAttribute("greeting", "Welcome to our Car Reservation System!!");
